28e783b213194fa652eec2808aafc77227fadc56
Harsh Pundir <harsh.pundir@ukg.com>
2024-01-28 10:53:03 +0530

    adding icons to navbar

--- src/staging_source/com.hpmv.static.widgets/navigatorBar.js
+++ src/staging_source/com.hpmv.static.widgets/navigatorBar.js
@@ -4,12 +4,19 @@ import "./css/nav.css";
 import { SiOpenstreetmap } from "react-icons/si";
 import { PiListNumbers } from "react-icons/pi";
 import { LuLogIn } from "react-icons/lu";
+import { FaUserLarge } from "react-icons/fa6";
+
 
 
 const navigatorBar = () => {
   return (
     <div className="nav-list">
-      <div className="left-nav-list left"></div>
+      <div className="left-nav-list left">
+      <Link to="/mapView" className="list-item"> 
+      {/* replace this link to user profile */}
+      <FaUserLarge  className="mobile-only"/>
+      </Link>
+      </div>
       <div className="right-nav-List right">
         <Link to="/mapView" className="list-item">
         <SiOpenstreetmap />
@@ -20,6 +27,7 @@ const navigatorBar = () => {
           <p>List View</p>
         </Link>
         <Link to="/listView" className="list-item" id="nav-login-element">
+        {/* replace this link to login page */}
         <LuLogIn/>
           <p>Log In</p>
         </Link>
